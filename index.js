719d710a7eda5f355bd010942d64414dc396c2ce
Chue Vang <chuyieng@gmail.com>
2023-07-16 12:45:17 -0500

    logo maker works, needs aligning

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/shapes.js')
+const Square = require('./lib/square.js');
+const Circle = require('./lib/circle.js');
+const Triangle = require('./lib/triangle.js');
 
+function shapeChoice(data) {
+    if (data.shape === 'square') {
+
+        let choiceSquare = new Square (data.textColor, data.text, data.shapeColor);
+        return choiceSquare.render();
+
+    }
+    if (data.shape === 'circle') {
+
+        let choiceCircle = new Circle (data.textColor, data.text, data.shapeColor);
+        return choiceCircle.render();
+
+    }
+    if (data.shape === 'triangle') {
+
+        let choiceTriangle = new Triangle (data.textColor, data.text, data.shapeColor);
+        return choiceTriangle.render();
+
+    }
+};
+ 
 inquirer.prompt([
     {
         type: 'input',
@@ -16,7 +39,8 @@ inquirer.prompt([
     {
         type: 'list',
         message: 'What shape do you desire?',
-        name: ['Square', 'Circle', 'Triangle'],
+        name: 'shape',
+        choices: ['square', 'circle', 'triangle'],
     },
     {
         type: 'input',
@@ -24,13 +48,9 @@ inquirer.prompt([
         name: 'shapeColor',
     },
 ]).then((data) => {
-    
-    const svgShape = shapes;
 
-    if (data.text.length < 3 || data.text.length > 3) {
-        console.log('Please enter 3 characters');
-    } else {
-        fs.writeFile('logo.svg', svgShape, (err) => err ? console.error(err) : console.log('Success!'));
-    }
+        const userSelection = shapeChoice(data);
+
+        fs.writeFile('logo.svg', userSelection, (err) => err ? console.error(err) : console.log('Success!'));
 
 });
